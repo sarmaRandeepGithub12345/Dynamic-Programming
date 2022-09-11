e5974a1c509a056678c8ef90ffbc2e8b345d0d76
Randeep Sarma <92437704+randeepsarma@users.noreply.github.com>
2022-09-11 17:50:05 +0530

    Create best_time_to_buy_and_sell_stock_III.cpp

---
+++ best_time_to_buy_and_sell_stock_III.cpp
@@ -0,0 +1,43 @@
+//2 pass
+int maxProfit(vector<int>& prices, int n)
+{
+ vector<int>profit(n,0);
+ int l=1000000,ma=0;
+  for(int i=0;i<n;i++){
+      if(l>prices[i])
+          l=prices[i];
+      else {
+          ma=max(ma,prices[i]-l);
+      }
+      profit[i]=ma;    
+  }
+  l=-1000000,ma=0;
+    int ans=0;
+  for(int i=n-1;i>=0;i--){
+      if(l<prices[i])l=prices[i];
+      else {
+          ma=max(ma,l-prices[i]);
+      }
+      profit[i]+=ma;
+      ans=max(ans,profit[i]);
+  }  
+    
+      return ans;
+}
+
+
+//1 pass
+
+int maxProfit(vector<int>& prices, int n)
+{
+    int b1=100000,p1=0,b2=100000,p2=0;
+ for(auto it:prices){
+     b1=min(b1,it);//first buy
+     p1=max(p1,it-b1);//first sell
+     b2=min(b2,it-p1);//second effective buy after minusing profit of previous
+     p2=max(p2,it-b2);//Overall profit
+ }
+   return p2;
+}
+
+
