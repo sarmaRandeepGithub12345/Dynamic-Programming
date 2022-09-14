56d742ecae2b8c3bd266db35cd3e29151beae054
Randeep Sarma <92437704+randeepsarma@users.noreply.github.com>
2022-09-14 17:35:24 +0530

    Update Longest_increasing_subsequence.cpp

--- Longest_increasing_subsequence.cpp
+++ Longest_increasing_subsequence.cpp
@@ -1,4 +1,5 @@
- int lengthOfLIS(vector<int>& arr) {
+//Solution 1
+int lengthOfLIS(vector<int>& arr) {
         int n=arr.size();
         vector<int>ans(n,1);
  
@@ -15,3 +16,27 @@
  }
     return omax;
     }
+//Solution 2
+int longestIncreasingSubsequence(int arr[], int n)
+{
+   // Write Your Code here
+    vector<int>temp;
+  temp.push_back(arr[0]);
+  int len=1;
+  for(int i=1;i<n;i++)
+  {
+      if(arr[i]>temp.back())
+      {
+          temp.push_back(arr[i]);
+          len++;
+      }
+      else
+      {
+          int idx=lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin();
+          temp[idx]=arr[i];
+      }
+  }
+    return len;
+}
+
+
